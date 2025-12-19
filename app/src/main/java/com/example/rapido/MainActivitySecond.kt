4e8dde1152e7f3abb8c4ecf6e061874294b3bdd7
SeinaruHi <vano.xmao@mail.ru>
2025-12-19 13:41:19 +0500

    initial commit

---
+++ app/src/main/java/com/example/rapido/MainActivitySecond.kt
@@ -0,0 +1,11 @@
+package com.example.rapido
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitySecond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_second)
+    }
+}
